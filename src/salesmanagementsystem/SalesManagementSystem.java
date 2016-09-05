4e8a4bdc768adda8da61aa711897498c4d4c42b7
hasa1990 <hasa1990@gmail.com>
2016-09-05 20:42:42 +0530

    test
    tes

--- src/salesmanagementsystem/SalesManagementSystem.java
+++ src/salesmanagementsystem/SalesManagementSystem.java
@@ -14,6 +14,6 @@ public class SalesManagementSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("kshdgf");
+        System.out.println(7765);
     }
 }
