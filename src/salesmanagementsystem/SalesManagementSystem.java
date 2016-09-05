f2d7dec482bb249e8e8caff8659bd0d4b03757e8
hasa1990 <hasa1990@gmail.com>
2016-09-05 20:41:43 +0530

    safdf
    asds

---
+++ src/salesmanagementsystem/SalesManagementSystem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salesmanagementsystem;
+
+/**
+ *
+ * @author hasangi
+ */
+public class SalesManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("kshdgf");
+    }
+}
